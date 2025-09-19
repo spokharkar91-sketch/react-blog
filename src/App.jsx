9024eb5bc5ac434e331b0760d6ba670b6a611092
suyash <spokharkar91@gmail.com>
2025-09-19 07:06:00 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function Header(){
+  return<h1 class="suyash">Hello React</h1>;
+}
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Header/>
+    </>
+  )
+}
+
+export default App
